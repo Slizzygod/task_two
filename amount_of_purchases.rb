13d9ea76fb5dc0aaba4d024be1e2a19e9a27ccb3
Slizzygod <Slizzygod@mail.ru>
2020-03-14 12:48:52 +0300

    Last

---
+++ amount_of_purchases.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+hash_goods = {}
+sum_cart = 0
+loop do
+  puts 'Введите наименование товара или "Stop"'
+  title = gets.chomp.to_s
+  break if title == 'Stop'
+
+  puts 'Введите цену товара'
+  price = gets.chomp.to_f
+  puts 'Введите количество товара'
+  quantity = gets.chomp.to_f
+  hash_goods[title] = { 'price' => price, 'quantity' => quantity }
+end
+
+hash_goods.each { |_name, hash| sum_cart += hash['price'] * hash['quantity'] }
+puts hash_goods
+puts "Итого: #{sum_cart}"
